6b62a50d89abc30f0676af4859981e1001b04fd4
Venh <venh.ha@gmail.com>
2024-05-26 00:13:21 +0700

    [Added] add function in ConversionUtils

--- lib/src/core/utils.dart
+++ lib/src/core/utils.dart
@@ -11,6 +11,13 @@ import 'package:http_parser/http_parser.dart';
 import 'base/debouncer.dart';
 import 'constants/types.dart';
 
+const _defaultSeparator = '.';
+const _defaultHundredAbbreviation = ''; // hundred
+const _defaultThousandAbbreviation = 'K'; // thousand
+const _defaultMillionAbbreviation = 'M'; // million
+const _defaultBillionAbbreviation = 'B'; // billion
+const _defaultTrillionAbbreviation = 'T'; // trillion
+
 class ValidationUtils {
   static bool isValidEmail(String email) {
     return RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(email);
@@ -375,12 +382,148 @@ class NetWorkUtils {
 }
 
 class ConversionUtils {
+  /// this function is used to convert double value to int value with ceil to 5
+  /// - If value is 0, return 0
+  /// - If value is 1-9, return 5 or 10
+  /// - If value is 10+, return ceil to 5 first two digits
+  ///
+  /// eg: 0 => 0, 4 => 5, 8 => 10, 1234 => 1500, 1234567 => 1500000
+  static int ceilTo5FirstTwoDigits(double value) {
+    //# 0 value
+    if (value == 0) return 0;
+
+    //# 1 digit value: 1-9
+    if (value < 10) {
+      if (value >= 5) return 10;
+      return 5;
+    }
+
+    //# 2+ digits value: >= 10
+    final String valueString = value.toStringAsFixed(0);
+    final String first = valueString[0];
+    final String second = valueString[1];
+
+    int firstNum = int.parse(first);
+    int secondNum = int.parse(second);
+
+    if (secondNum >= 5 && secondNum <= 9) {
+      firstNum++;
+      secondNum = 0;
+    } else if (secondNum < 5 && secondNum >= 0) {
+      secondNum = 5;
+    }
+
+    final roundedValue = firstNum.toString() + secondNum.toString();
+    final roundedValueString = roundedValue + '0' * (valueString.length - 2);
+
+    return int.parse(roundedValueString);
+  }
+
+  /// This function is used to format currency with (short name) abbreviation
+  ///
+  /// The [extra] just work only the length of value is greater than 3 (start from 1000).
+  /// Example:
+  /// - 1234 (fraction = 0) => 1K, (fraction = 1) => 1.2K, (fraction = 2) => 1.23K
+  /// - 123456 (fraction = 0) => 123K, (fraction = 1) => 123.4K, (fraction = 2) => 123.45K
+  /// - 12 (fraction = 2, extra = true) => 12
+  /// - 1234 (fraction = 4, extra = true) => 1.2340K
+  static String formatCurrencyWithAbbreviation(
+    double value, {
+    int fraction = 0,
+    String separator = _defaultSeparator,
+    bool extra = false,
+    String hundredAbbreviation = _defaultHundredAbbreviation,
+    String thousandAbbreviation = _defaultThousandAbbreviation,
+    String millionAbbreviation = _defaultMillionAbbreviation,
+    String billionAbbreviation = _defaultBillionAbbreviation,
+    String trillionAbbreviation = _defaultTrillionAbbreviation,
+  }) {
+    int maxDigitsCanTake(int length) {
+      if (length < 4) return 0;
+
+      switch (length) {
+        case 4 || 7 || 10 || 13:
+          return length - 1; // eg: 1234 => 3, 1234567 => 6
+        case 5 || 8 || 11 || 14:
+          return length - 2; // eg: 12345 => 3, 12345678 => 6
+        case 6 || 9 || 12 || 15:
+          return length - 3; // eg: 123456 => 3, 123456789 => 6
+        default:
+          throw const FormatException('Invalid length');
+      }
+    }
+
+    String mainValue(String value, int length) {
+      if (value.length < 4) return value;
+
+      final int endSub;
+      switch (length) {
+        case 4 || 7 || 10 || 13:
+          endSub = 1;
+        case 5 || 8 || 11 || 14:
+          endSub = 2;
+        case 6 || 9 || 12 || 15:
+          endSub = 3;
+        default:
+          throw const FormatException('Invalid length');
+      }
+      return value.substring(0, endSub);
+    }
+
+    /// this [showExtra] is used to show extra zeros if the fraction is greater than the maxDigitsCanTake
+    String subValue(String value, int fraction, String separator, bool showExtra) {
+      if (fraction == 0 || value.length < 4) return '';
+
+      final int startSub;
+      switch (value.length) {
+        case 4 || 7 || 10 || 13:
+          startSub = 1;
+        case 5 || 8 || 11 || 14:
+          startSub = 2;
+        case 6 || 9 || 12 || 15:
+          startSub = 3;
+        default:
+          throw const FormatException('Invalid length');
+      }
+
+      final max = maxDigitsCanTake(value.length);
+      if (fraction <= max) {
+        return separator + value.substring(startSub, startSub + fraction);
+      } else {
+        if (!showExtra) return separator + value.substring(startSub, startSub + max);
+        return separator + value.substring(startSub, startSub + max) + '0' * (fraction - max);
+      }
+    }
+
+    String suffix(int length) {
+      if (length > 12) {
+        return trillionAbbreviation;
+      } else if (length > 9) {
+        return billionAbbreviation;
+      } else if (length > 6) {
+        return millionAbbreviation;
+      } else if (length > 3) {
+        return thousandAbbreviation;
+      } else if (length > 0) {
+        return hundredAbbreviation;
+      }
+      throw const FormatException('Invalid length');
+    }
+
+    final valueString = value.toStringAsFixed(0);
+    assert(valueString.isNotEmpty, 'value must not be empty');
+
+    final int length = valueString.length;
+    if (length < 4) return valueString;
+
+    return mainValue(valueString, length) + subValue(valueString, fraction, separator, extra) + suffix(length);
+  }
+
   String roundDouble(double value) {
     String valueString = value.toStringAsFixed(0);
 
     return valueString;
   }
-
 
   static String formatCurrency(int value, {bool showUnit = true, String? unit = 'đ'}) {
     var f = NumberFormat.decimalPattern();
--- test/all_test.dart
+++ test/all_test.dart
@@ -1,53 +1,53 @@
+import 'package:flutter_test/flutter_test.dart';
 // ignore_for_file: avoid_print
 
-// import 'dart:math';
-
-import 'package:flutter_test/flutter_test.dart';
-
-const double x1 = 1234.38456243;
-const double x2 = 834576.9877865;
-const double x3 = 3945867435.9877865;
-const double x4 = 8437563.9877865;
-const double x5 = 834675.9877865;
-const double x6 = 934756.9877865;
-const double x7 = 283458976.9877865;
-const double x8 = 433.9877865;
-
-void main0() {
-  test('test roundDouble', () async {
-    print(roundDouble(x1));
-    print('--------------------------------------------');
-    print(roundDouble(x2));
-    print('--------------------------------------------');
-    print(roundDouble(x3));
-    print('--------------------------------------------');
-    print(roundDouble(x4));
-    print('--------------------------------------------');
-    print(roundDouble(x5));
-    print('--------------------------------------------');
-    print(roundDouble(x6));
-    print('--------------------------------------------');
-    print(roundDouble(x7));
-    print('--------------------------------------------');
-    print(roundDouble(x8));
+void main() {
+  test('test formatWithSuffix', () async {
+    print(formatCurrencyWithAbbreviation('12', fraction: 10));
+    print(formatCurrencyWithAbbreviation('1', fraction: 10, extra: true));
+    print(formatCurrencyWithAbbreviation('12', fraction: 10, extra: true));
+    print(formatCurrencyWithAbbreviation('123', fraction: 10, extra: true));
+    print(formatCurrencyWithAbbreviation('1234', fraction: 4, extra: true));
+    // print(formatCurrencyWithAbbreviation('1234'));
+    // print('--------------------------------------------');
+    // print(formatCurrencyWithAbbreviation('123456', fraction: 1));
+    // print(formatCurrencyWithAbbreviation('123456134', fraction: 2));
+    // print(formatCurrencyWithAbbreviation('13245643132', fraction: 3));
+    // print(formatCurrencyWithAbbreviation('1236356546', fraction: 4));
+    // print('--------------------------------------------');
+    // print(ceilTo5FirstTwoDigits(1234));
+    // print(ceilTo5FirstTwoDigits(123456));
+    // print(ceilTo5FirstTwoDigits(123456134));
+    // print(ceilTo5FirstTwoDigits(13245643132));
+    // print(ceilTo5FirstTwoDigits(1236356546));
+    // print('--------------------------------------------');
   });
 }
 
-int? roundDouble(double value) {
+const _defaultSeparator = '.';
+const _defaultHundredAbbreviation = ''; // hundred
+const _defaultThousandAbbreviation = 'K'; // thousand
+const _defaultMillionAbbreviation = 'M'; // million
+const _defaultBillionAbbreviation = 'B'; // billion
+const _defaultTrillionAbbreviation = 'T'; // trillion
+
+int ceilTo5FirstTwoDigits(double value) {
+  //# 0 value
+  if (value == 0) return 0;
+
+  //# 1 digit value: 1-9
+  if (value < 10) {
+    if (value >= 5) return 10;
+    return 5;
+  }
+
+  //# 2+ digits value: >= 10
   final String valueString = value.toStringAsFixed(0);
-  if (valueString.length < 3) return null;
+  final String first = valueString[0];
+  final String second = valueString[1];
 
-  String first = valueString[0];
-  String second = valueString[1];
-  // String third = valueString[2];
-  print('firstPart: $first');
-  print('second: $second');
-  // print('third: $third');
-
-  int? firstNum = int.tryParse(first);
-  int? secondNum = int.tryParse(second);
-  // int? thirdNum = int.tryParse(third);
-  if (firstNum == null || secondNum == null) return null; //! review
+  int firstNum = int.parse(first);
+  int secondNum = int.parse(second);
 
   if (secondNum >= 5 && secondNum <= 9) {
     firstNum++;
@@ -58,120 +58,97 @@ int? roundDouble(double value) {
 
   final roundedValue = firstNum.toString() + secondNum.toString();
   final roundedValueString = roundedValue + '0' * (valueString.length - 2);
-  print(formatWithSuffix(roundedValueString));
+  print('roundedValueString: ${formatCurrencyWithAbbreviation(roundedValueString, fraction: 1)}');
 
-  return int.tryParse(roundedValueString);
+  return int.parse(roundedValueString);
 }
 
-String round(double value) {
-  final String valueString = value.toStringAsFixed(0);
-  String suffix = '';
+String formatCurrencyWithAbbreviation(
+  String value, {
+  int fraction = 0,
+  String separator = _defaultSeparator,
+  bool extra = false,
+  String hundredAbbreviation = _defaultHundredAbbreviation,
+  String thousandAbbreviation = _defaultThousandAbbreviation,
+  String millionAbbreviation = _defaultMillionAbbreviation,
+  String billionAbbreviation = _defaultBillionAbbreviation,
+  String trillionAbbreviation = _defaultTrillionAbbreviation,
+}) {
+  int maxDigitsCanTake(int length) {
+    if (length < 4) return 0;
 
-  if (valueString.length > 12) {
-    suffix = 'T';
-  } else if (valueString.length > 9) {
-    suffix = 'B';
-  } else if (valueString.length > 6) {
-    suffix = 'M';
-  } else if (valueString.length > 3) {
-    suffix = 'K';
+    switch (length) {
+      case 4 || 7 || 10 || 13:
+        return length - 1; // eg: 1234 => 3, 1234567 => 6
+      case 5 || 8 || 11 || 14:
+        return length - 2; // eg: 12345 => 3, 12345678 => 6
+      case 6 || 9 || 12 || 15:
+        return length - 3; // eg: 123456 => 3, 123456789 => 6
+      default:
+        throw const FormatException('Invalid length');
+    }
   }
-  return suffix;
-}
 
-void main() {
-  test('test formatWithSuffix', () async {
-    print(formatWithSuffix('1234'));
-    print('--------------------------------------------');
-    print(formatWithSuffix('123456'));
-    print(formatWithSuffix('123456134', 1));
-    print(formatWithSuffix('13245643132', 1));
-    print(formatWithSuffix('1236356546', 1));
-    print('--------------------------------------------');
-  });
-}
+  String mainValue(String value, int length) {
+    if (value.length < 4) return value;
 
-String formatWithSuffix(String value, [int fraction = 0, String separator = '.', bool extra = false]) {
+    final int endSub;
+    switch (length) {
+      case 4 || 7 || 10 || 13:
+        endSub = 1;
+      case 5 || 8 || 11 || 14:
+        endSub = 2;
+      case 6 || 9 || 12 || 15:
+        endSub = 3;
+      default:
+        throw const FormatException('Invalid length');
+    }
+    return value.substring(0, endSub);
+  }
+
+  /// this [showExtra] is used to show extra zeros if the fraction is greater than the maxDigitsCanTake
+  String subValue(String value, int fraction, String separator, bool showExtra) {
+    if (fraction == 0 || value.length < 4) return '';
+
+    final int startSub;
+    switch (value.length) {
+      case 4 || 7 || 10 || 13:
+        startSub = 1;
+      case 5 || 8 || 11 || 14:
+        startSub = 2;
+      case 6 || 9 || 12 || 15:
+        startSub = 3;
+      default:
+        throw const FormatException('Invalid length');
+    }
+
+    final max = maxDigitsCanTake(value.length);
+    if (fraction <= max) {
+      return separator + value.substring(startSub, startSub + fraction);
+    } else {
+      if (!showExtra) return separator + value.substring(startSub, startSub + max);
+      return separator + value.substring(startSub, startSub + max) + '0' * (fraction - max);
+    }
+  }
+
+  String suffix(int length) {
+    if (length > 12) {
+      return trillionAbbreviation;
+    } else if (length > 9) {
+      return billionAbbreviation;
+    } else if (length > 6) {
+      return millionAbbreviation;
+    } else if (length > 3) {
+      return thousandAbbreviation;
+    } else if (length > 0) {
+      return hundredAbbreviation;
+    }
+    throw const FormatException('Invalid length');
+  }
+
+  assert(value.isNotEmpty, 'value must not be empty');
   if (value.length < 4) return value;
 
   final int length = value.length;
   return mainValue(value, length) + subValue(value, fraction, separator, extra) + suffix(length);
-}
-
-int maxDigitsCanTake(int length) {
-  if (length < 4) return 0;
-
-  switch (length) {
-    case 4 || 7 || 10 || 13:
-      return length - 1; // eg: 1234 => 3, 1234567 => 6
-    case 5 || 8 || 11 || 14:
-      return length - 2; // eg: 12345 => 3, 12345678 => 6
-    case 6 || 9 || 12 || 15:
-      return length - 3; // eg: 123456 => 3, 123456789 => 6
-    default:
-      throw const FormatException('Invalid length');
-  }
-}
-
-String mainValue(String value, int length) {
-  if (value.length < 4) return value;
-
-  final int endSub;
-  switch (length) {
-    case 4 || 7 || 10 || 13:
-      endSub = 1;
-    case 5 || 8 || 11 || 14:
-      endSub = 2;
-    case 6 || 9 || 12 || 15:
-      endSub = 3;
-    default:
-      throw const FormatException('Invalid length');
-  }
-  return value.substring(0, endSub);
-}
-
-String subValue(String value, int fraction, String separator, bool extra) {
-  if (fraction == 0) return '';
-
-  if (value.length < 4) return '';
-
-  final int startSub;
-  switch (value.length) {
-    case 4 || 7 || 10 || 13:
-      startSub = 1;
-    case 5 || 8 || 11 || 14:
-      startSub = 2;
-    case 6 || 9 || 12 || 15:
-      startSub = 3;
-    default:
-      throw const FormatException('Invalid length');
-  }
-
-  final max = maxDigitsCanTake(value.length);
-  if (fraction <= max) {
-    return separator + value.substring(startSub, startSub + fraction);
-  } else {
-    if (!extra) return separator + value.substring(startSub, startSub + max);
-    return separator + value.substring(startSub, startSub + max) + '0' * (fraction - max);
-  }
-}
-
-const thousandCurrencyUnit = 'K'; // stands for thousand
-const millionCurrencyUnit = 'M'; // stands for million
-const billionCurrencyUnit = 'B'; // stands for billion
-const trillionCurrencyUnit = 'T'; // stands for trillion
-
-String suffix(int length) {
-  if (length < 4) return '';
-
-  if (length > 12) {
-    return trillionCurrencyUnit;
-  } else if (length > 9) {
-    return billionCurrencyUnit;
-  } else if (length > 6) {
-    return millionCurrencyUnit;
-  } else if (length > 3) {
-    return thousandCurrencyUnit;
-  }
-  throw const FormatException('Invalid length');
 }
